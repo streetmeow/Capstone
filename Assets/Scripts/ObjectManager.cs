f39135f5b9a2093073f8b422434045abf59826e4
streetmeow <joohee2008@cau.ac.kr>
2022-12-02 17:36:57 +0900

    object pooling

--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -1,18 +1,85 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject pathPrefab;
+    public GameObject destinationPrefab;
+    public GameObject questPrefab;
+
+    private GameObject[] paths;
+    private GameObject[] destinations;
+    private GameObject[] quests;
+    private GameObject[] targetPool;
+
+    private void Awake()
     {
-        
+        paths = new GameObject[100];
+        destinations = new GameObject[50];
+        quests = new GameObject[50];
+        Generate();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Generate()
     {
-        
+        for (int i = 0; i < paths.Length; i++)
+        {
+            paths[i] = Instantiate(pathPrefab);
+            paths[i].SetActive(false);
+        }
+
+        for (int i = 0; i < destinations.Length; i++)
+        {
+            destinations[i] = Instantiate(destinationPrefab);
+            destinations[i].SetActive(false);
+        }
+
+        for (int i = 0; i < quests.Length; i++)
+        {
+            quests[i] = Instantiate(questPrefab);
+            quests[i].SetActive(false);
+        }
+    }
+
+    public GameObject MakeObject(string type)
+    {
+        switch (type)
+        {
+            case "path":
+                targetPool = paths;
+                break;
+            case "destination":
+                targetPool = destinations;
+                break;
+            case "quest":
+                targetPool = quests;
+                break;
+        }
+
+        for (int i = 0; i < targetPool.Length; i++)
+        {
+            if (!targetPool[i].activeSelf)
+            {
+                targetPool[i].SetActive(true);
+                return targetPool[i];
+            }
+        }
+
+        return null;
+    }
+
+    public void DestinationReset()
+    {
+        for (int i = 0; i < destinations.Length; i++)
+        {
+            destinations[i].SetActive(false);
+        }
+
+        for (int i = 0; i < paths.Length; i++)
+        {
+            paths[i].SetActive(false);
+        }
     }
 }
